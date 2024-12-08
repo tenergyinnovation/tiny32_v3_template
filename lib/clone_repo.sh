7d03d9f56d729b24ac75f7989c62e0ba0ee19115
Tenergy Innovation <45764087+tenergyinnovation@users.noreply.github.com>
2024-12-08 16:47:54 +0700

    Add 1.clone_repo.sh 2.Wokwi simulate

---
+++ lib/clone_repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script clones a git repository
+
+# Define the repository URL
+REPO_URL1="https://github.com/tenergyinnovation/tiny32_v3.git"
+
+# Define the target directory (optional)
+TARGET_DIR="repository"
+
+# Clone the repository
+git clone $REPO_URL1
+
+
